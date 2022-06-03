f7e3a1cc044125aac32ba53980ed78a7d280fde4
Martin Emil <mar10martinemil@gmail.com>
2022-06-03 18:29:19 +0200

    removed getting all apps route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :apps
+    resources :apps, :except => :index
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
